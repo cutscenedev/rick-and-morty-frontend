a25cffb222556b387abdd751a5609a566cbe7bf7
Artur Reshetnikov <cutscenedev@gmail.com>
2024-06-23 21:29:19 +0200

    feat: add dummy requests for all three required endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,69 @@ import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
+interface Character {
+  name: string
+}
+
+interface User {
+  userName: string
+  userSettings: {
+    favoriteCharacters: string[]
+  }
+}
+
 function App() {
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState<null | Character[]>(null);
+  const [user, serUser] = useState<null | User>(null);
+
+  const userName = `userName-27`;
+  const newFavoriteCharacters = new Array(5).fill(1).map((c, i) => `${i}`)
+
+  async function updateFavoriteCharacters() {
+    const response = await fetch('http://localhost:4000/graphql/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        query: `mutation UpdateFavoriteCharacters($userName: String!, $newFavoriteCharacters: [String!]!) {
+          updateFavoriteCharacters(userName: $userName, newFavoriteCharacters: $newFavoriteCharacters) {
+            userName
+            userSettings {
+              favoriteCharacters
+            }
+          }
+        }`,
+        variables: { userName, newFavoriteCharacters }
+      }),
+    }).then(r => r.json());
+
+    serUser(response.data.updateFavoriteCharacters);
+  }
+
+  async function login() {
+    const response = await fetch('http://localhost:4000/graphql/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        query: `mutation Login($userName: String!) {
+          login(userName: $userName) {
+            userName
+            userSettings {
+              favoriteCharacters
+            }
+          }
+        }`,
+        variables: { userName }
+      }),
+    }).then(r => r.json());
+
+    serUser(response.data.login);
+  }
 
   async function loadCharacters() {
     const response = await fetch('http://localhost:4000/graphql/ramproxy', {
@@ -39,12 +100,20 @@ function App() {
   }
 
   useEffect(() => {
-    loadCharacters()
+    (async () => {
+      await login();
+      await loadCharacters();
+      await updateFavoriteCharacters();
+    })()
   }, [])
 
   return (
     <div className="App">
-      test
+      CHARACTERS: {characters !== null && characters.map(c => c.name)}
+      <br />
+      USER: {user?.userName}
+      <br />
+      chars: {user?.userSettings.favoriteCharacters.map(c => c)}
     </div>
   );
 }
