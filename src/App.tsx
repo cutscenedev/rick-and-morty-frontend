b5135732f89e1c8a4569a955768fb3e80277d230
Artur Reshetnikov <cutscenedev@gmail.com>
2024-06-23 15:58:11 +0200

    feat: add fetching of real RaM characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:4000/graphql", {
+  const [characters, setCharacters] = useState([]);
+
+  async function loadCharacters() {
+    const response = await fetch('http://localhost:4000/graphql/ramproxy', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json",
+        "Accept": "application/json",
       },
-      body: JSON.stringify({ query: "{ hello }" }),
-    })
-      .then(r => r.json())
-      .then(data => console.log("data returned:", data))
-  }, []);
+      body: JSON.stringify({
+        query: `{
+          characters(page: 1) {
+            results {
+              image
+              name
+              species
+              gender
+              status
+              origin {
+                name
+                dimension
+              }
+              episode {
+                name
+                air_date
+              }
+            }
+          }
+        }`,
+      }),
+    }).then(r => r.json());
+
+    setCharacters(response.data.characters.results)
+  }
+
+  useEffect(() => {
+    loadCharacters()
+  }, [])
 
   return (
     <div className="App">
