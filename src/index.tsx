dff4f2b779db967e6fa5d95a069a3707511fd703
Artur Reshetnikov <cutscenedev@gmail.com>
2024-07-01 01:32:03 +0200

    refactor: add mobx config

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import './mobx-config';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
